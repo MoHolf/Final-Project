e2c2b3f0e3649392e674e9af51f06db70942d5fd
Mohamed Hamza <ma-hamza@wiu.edu>
2019-05-01 23:20:54 -0500

    Latest

--- FinalProjectShiny/server.R
+++ FinalProjectShiny/server.R
@@ -7,15 +7,43 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
+  
+  Data <- read.csv ( "Sex_Offenders.csv")
+  
+  
+  SORecord <- data.frame(Data)
+  
+  x <- SORecord$WEIGHT
+  y <- SORecord$AGE
+  
+  Data$WEIGHTSP <- ifelse(Data$WEIGHT - 320 > 0, Data$WEIGHT - 320, 0)
+ 
+  
+  model1 <- lm(AGE ~ WEIGHT, data = Data)
+  model2 <- lm(AGE ~ WEIGHTSP + WEIGHT, data = Data)
+  
+  model1pred <- reactive({
+    
+    xInput <- input$bins2
+    predict(model1, newdata = data.frame(WEIGHT = xInput))
+    
+  })
+  
+  model2pred <- reactive({
+    
+    xInput <- input$bins2
+    predict(model2, newdata = data.frame(WEIGHT = xInput, WEIGHTSP = ifelse(xInput - 320 > 0,xInput -320, 0) ))
+    
+  })
+  
   output$SOPlot1 <- renderPlot({
     
+
     
     Data <- read.csv ( "Sex_Offenders.csv")
     
     
     SORecord <- data.frame(Data)
-    
-    
     
     x <- SORecord$WEIGHT
     
@@ -42,8 +70,24 @@ shinyServer(function(input, output) {
           bg = "lightblue",
           
           col = "black", cex = 2, pch = 21, frame = FALSE)
+    xInput <- input$bins2
+    Data$WEIGHTSP <- ifelse(Data$WEIGHT - 320 > 0, Data$WEIGHT - 320, 0)
     
+    model1 <- lm(y ~ x)
+    model2 <- lm(y ~ WEIGHTSP + WEIGHT, data = Data)
     
+    if(input$showModel1){
+      abline(model1, col = "red", lwd = 2)
+    }
+    
+    if(input$showModel2){
+      model2lines <- predict(model2, newdata = data.frame(WEIGHT = 0:1073, WEIGHTSP = ifelse(0:1073 - 320 > 0, 0:1073 - 320, 0)))
+      lines(0:1073, model2lines, col = "blue", lwd = 2)
+    }
+    
+    legend(25, 250, c("Model 1 Prediction","Model 2 Prediction"), pch = 21, col = c("red","blue"), bty = "n", cex = 1.2)
+    points(xInput, model1pred(), col = "red", pch = 16, cex = 2)
+    points(xInput, model2pred() , col = "blue", pch = 16, cex = 2)
     
     
   })
@@ -55,8 +99,6 @@ shinyServer(function(input, output) {
     
     
     SORecord <- data.frame(Data)
-    
-    
     
     x <- SORecord$WEIGHT
     
@@ -101,6 +143,13 @@ shinyServer(function(input, output) {
       
       lines (c(x[i], x[i]), c(e[i],0), col = "red", lwd = 2)
     
+  })
+  
+  output$pred1 <- renderText({
+    model1pred()
+  })
+  output$pred2 <- renderText({
+    model2pred()
   })
   
 })
--- FinalProjectShiny/ui.R
+++ FinalProjectShiny/ui.R
@@ -32,7 +32,13 @@ shinyUI(fluidPage(
                   min = 0,
                   max = 500,
                   value = 0),
-      
+      sliderInput("bins2",
+                  "Value:",
+                  min = 0,
+                  max = 620,
+                  value = 0),
+      checkboxInput("showModel1","Show/Hide Model 1", value = TRUE),
+      checkboxInput("showModel2","Show/Hide Model 2", value = TRUE),
       h6("Author:"),
       h6("Mohamed Hamza"),
       h6("Hongwei Wang"),
@@ -47,13 +53,17 @@ shinyUI(fluidPage(
     
     
     
-    
-    # Show a plot of the generated distribution
     mainPanel(
       list(tags$head(tags$style("body {background-color: #2f6640; }"))),
       plotOutput("SOPlot1"),
       
-      plotOutput("SOPlot2")
+      plotOutput("SOPlot2"),
+      
+      h3("Predicted Age from Model 1:"),
+      textOutput("pred1"),
+      h3("Predicted Age from Model 2:"),
+      textOutput("pred2")
+      
     )
   )
 ))
